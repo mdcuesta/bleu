a5fabf9407b21ee6a6b227030f7a9745eb888942
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2012-06-10 00:07:00 +0800

    modified disqus script

--- Bleu.Disqus/DisqusHelper.cs
+++ Bleu.Disqus/DisqusHelper.cs
@@ -13,7 +13,7 @@ namespace Bleu.Disqus
         {
             var settingsValue = System.Configuration.ConfigurationManager.AppSettings["disqusid"];
             var linkFormat = 
-                "<script type=\"text/javascript\" src=\"http://disqus.com/forums/{0}/embed.js\"></script><noscript><a href=\"http://{1}.disqus.com/?url=ref\">View the discussion thread.</a></noscript><a href=\"http://disqus.com\" class=\"dsq-brlink\">blog comments powered by <span class=\"logo-disqus\">Disqus</span></a>";
+                "<div id=\"disqus_thread\"></div><script type=\"text/javascript\" src=\"http://disqus.com/forums/{0}/embed.js\"></script><noscript><a href=\"http://{1}.disqus.com/?url=ref\">View the discussion thread.</a></noscript><a href=\"http://disqus.com\" class=\"dsq-brlink\">blog comments powered by <span class=\"logo-disqus\">Disqus</span></a>";
 
             if (!string.IsNullOrEmpty(settingsValue))
             {          
